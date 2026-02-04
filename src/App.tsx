61814116e4ac01dd8bd247a1e4ec2b40f01967d3
seblama <sebastian.alama@gmail.com>
2026-02-04 18:58:10 -0300

    Add configuration and main app files

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Registration from "./pages/Registration";
+import TableView from "./pages/TableView";
+import Coordinadores from "./pages/Coordinadores";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/table" element={<TableView />} />
+          <Route path="/coordinadores" element={<Coordinadores />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
